3ec7977f4e8550b616d1243a4a344629bea48957
Jordan Davidson <Thatoneemail@gmail.com>
2015-03-21 13:20:21 -0600

    added conditional install and cloud 9

--- buid.sh
+++ buid.sh
@@ -1,11 +1,33 @@
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get -y install build-essential openssl libssl-dev pkg-config libxml2-dev
-wget http://node-arm.herokuapp.com/node_0.10.36_armhf.deb
-cd /usr/local
-sudo tar xzvf ~/node-v0.8.17-linux-arm-pi.tar.gz --strip=1
-mkdir workspace
+sudo apt-get -y install build-essential openssl libssl-dev pkg-config libxml2-dev vim
+#node
+if [ $(node -v) ]; then
+	echo "node already installed skipping installation"
+else
+	wget http://node-arm.herokuapp.com/node_0.10.36_armhf.deb
+	sudo dpkg -i node_0.10.36_armf.deb
+	rm node_0.10.36_armhf.deb
+fi
+
+cd ~
+mkdir ~/workspace
+
 #cloud9
-git clone https://github.com/ajaxorg/cloud9.git ~/workspace/cloud9
-cd ~/workspace/cloud9 && mkdir node_modules && cd node_modules
-git clone https://github.com/ajaxorg/node-libxml.git libxml
-cd libxml && git checkout v0.0.7 && git submodule init && git submodule update
+if [ -d "/home/pi/workspace/cloud9" ]; then
+	echo "cloud9 already installed skipping installation"
+else
+	git clone https://github.com/ajaxorg/cloud9.git ~/workspace/cloud9
+	cd ~/workspace/cloud9 && mkdir node_modules && cd node_modules
+	#libxml
+	git clone https://github.com/ajaxorg/node-libxml.git libxml
+	cd libxml && git checkout v0.0.7 && git submodule init && git submodule update
+	sed "s/'-msse2' , //g" < ~/workspace/cloud9/node_modules/libxml/support/o3/wscript > ~/workspace/cloud9/node_modules/libxml/support/o3/wscript
+	cd ~/workspace/cloud9/node_modules/libxml
+	sudo npm install -g
+	cd ~/cloud9
+	sudo npm install
+	rm -r ./node_modules/vfs-local
+	npm install vfs-local@0.3.4
+	bin/cloud9.sh -l 0.0.0.0 &
+fi
+
